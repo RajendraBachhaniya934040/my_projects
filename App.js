6a6477f1b3ddf002e9f08e9ad3672a09f1623e34
RajendraBachhaniya934040 <rajendra.bachhaniya@gemsessence.com>
2020-04-28 15:39:58 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+// const CodePush = require('code-push');
+// const codePush = new CodePush("6ac5a20a3822bf70507eafe2bd3eb4645a559da6");
+
+class App extends Component {
+  render() {
+    return <Text>HIII</Text>;
+  }
+}
+
+export default App;
